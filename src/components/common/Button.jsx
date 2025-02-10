bad27b41ff7d7fc9a62ba9f19e81ccb5d39e36bd
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2025-02-10 15:10:00 +0900

    feat: 버튼 컴포넌트 생성

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Button.scss';
+
+const Button = ({ btnOn, buttonSize, buttonColor, children, action }) => {
+  return (
+    <button
+      onClick={() => action()}
+      disabled={btnOn}
+      className={`${buttonSize} ${buttonColor}`}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
