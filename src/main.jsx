b08383bf1d3a7f1c13ea79f8777179254947fa74
Kuheewon <119719994+HeHelee@users.noreply.github.com>
2025-02-10 15:07:56 +0900

    chore: 불필요한 내용 삭제

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,8 @@
-// 메인 앱 컴포넌트
 import React from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Layout from './components/Layout';
-import EmployeeRegistration from './components/EmployeeRegistration';
-import './App.css';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        {/* 기본 경로는 로그인 페이지로 설정 */}
-        <Route path="/" element={<Login />} />
-        {/* 대시보드 레이아웃 내부의 라우팅 */}
-        <Route
-          path="/dashboard/*"
-          element={
-            <Layout>
-              <Routes>
-                <Route path="register" element={<EmployeeRegistration />} />
-              </Routes>
-            </Layout>
-          }
-        />
-      </Routes>
-    </Router>
-  );
-}
-// React 18의 새로운 렌더링 방식으로 앱 마운트
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
