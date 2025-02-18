585bd93e9071b2c4ba87e485c34864a490eeb3db
HeHelee <selly3109@naver.com>
2025-02-18 10:38:23 +0900

    feat: header 로고에 링크 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../styles/Header.scss';
 import logoImage from '/logo.png';
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate('/dashboard');
+  };
   return (
     <header className="header">
       <div className="header__container">
-        <div className="header__logo-wrapper">
+        <div
+          className="header__logo-wrapper"
+          onClick={handleLogoClick}
+          style={{ cursor: 'pointer' }}
+        >
           <img src={logoImage} alt="Seohan" className="header__logo" />
         </div>
       </div>
